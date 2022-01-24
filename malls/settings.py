adca956d68fd7eb234ef8d649ad8c043e9407df5
borshasafia <97159895+borshasafia@users.noreply.github.com>
2022-01-24 10:33:24 +0600

    3

--- malls/settings.py
+++ malls/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'malls.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         #edit
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'myproject',
-        'USER': 'postgres',
-        'PASSWORD': '16258015gW@',
-        'HOST': 'localhost'
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'myproject',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '16258015gW@',
+        # 'HOST': 'localhost'
         #edit done
     }
 }
