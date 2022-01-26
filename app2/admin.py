6ae25e5cfc058c62a24a151216c77eb467f321d8
borshasafia <97159895+borshasafia@users.noreply.github.com>
2022-01-26 16:20:29 +0600

    3

--- app2/admin.py
+++ app2/admin.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
-from .models import Feature
+from .models import Feature, Test
 from .models import ShoppingMalls
 from .models import Product
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Test)
 class FeatureAdmin(admin.ModelAdmin):pass
 
 admin.site.register(Feature)
--- app2/models.py
+++ app2/models.py
@@ -3,6 +3,31 @@ from django.db import models
 from django.db.models import Model
 
 # Create your models here.
+class Test(models.Model):
+  #owner
+    title_new = models.CharField(max_length=200)
+    description = models.TextField(null=False)
+    division = models.CharField(max_length=200)
+    address = models.TextField(null=False)
+    
+    category_1 = models.CharField(max_length=200)
+    category_2 = models.CharField(max_length=200)
+    category_3 = models.CharField(max_length=200)
+    category_4 = models.CharField(max_length=200)
+    category_5 = models.CharField(max_length=200)
+    category_6 = models.CharField(max_length=200)
+    category_7 = models.CharField(max_length=200)
+    # featured_image
+    website_link = models.CharField(max_length=1000)
+    facebook_link = models.CharField(max_length=1000)
+    other_link = models.CharField(max_length=1000)
+    
+    division = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='uploads/')
+    vote_total = models.IntegerField(default=0)
+    vote_ratio = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 class Feature(models.Model):
     name: models.CharField(max_length=100)
     details: models.CharField(max_length=500)
@@ -10,11 +35,25 @@ class Feature(models.Model):
         return self.name
 class ShoppingMalls(models.Model):
    #owner
-    title = models.CharField(max_length=200)
+    title_new = models.CharField(max_length=200)
     description = models.TextField(null=False)
+    division = models.CharField(max_length=200)
+    address = models.TextField(null=False)
+    
+    category_1 = models.CharField(max_length=200,null=True,blank=True)
+    category_2 = models.CharField(max_length=200,null=True,blank=True)
+    category_3 = models.CharField(max_length=200,null=True,blank=True)
+    category_4 = models.CharField(max_length=200,null=True,blank=True)
+    category_5 = models.CharField(max_length=200,null=True,blank=True)
+    category_6 = models.CharField(max_length=200,null=True,blank=True)
+    category_7 = models.CharField(max_length=200,null=True,blank=True)
     # featured_image
-    demo_link = models.CharField(max_length=1000)
-    source_link = models.CharField(max_length=1000)
+    website_link = models.CharField(max_length=1000,null=True,blank=True)
+    facebook_link = models.CharField(max_length=1000,null=True,blank=True)
+    other_link = models.CharField(max_length=1000,null=True,blank=True)
+    
+    division = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='uploads/',null=True)
     vote_total = models.IntegerField(default=0)
     vote_ratio = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
