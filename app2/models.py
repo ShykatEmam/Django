0068723d8b1f0dae6c0e93d122480981fcd481bb
borshasafia <97159895+borshasafia@users.noreply.github.com>
2022-01-26 11:58:10 +0600

    Update models.py

--- app2/models.py
+++ app2/models.py
@@ -8,7 +8,6 @@ class Feature(models.Model):
 
 class ShoppingMalls(models.Model):
     mallname: models.TextField(max_length=100)
-    
     product1: models.CharField(max_length=75)
     product2: models.CharField(max_length=75)
     product3: models.CharField(max_length=75)
@@ -16,17 +15,11 @@ class ShoppingMalls(models.Model):
     product5: models.CharField(max_length=75)
     product6: models.CharField(max_length=75)
     product7: models.CharField(max_length=75)
-    
     link1: models.URLField(max_length=200)
     link2: models.URLField(max_length=200)
     link3: models.EmailField(max_length=100)
     link4: models.URLField(max_length=200)
-    
     division: models.CharField(max_length=50)
     location: models.URLField(max_length=200)
-    
     images: models.ImageField(upload_to='upload_to/')
-    
     mall_description: models.CharField(max_length=1000)
-    
-    
