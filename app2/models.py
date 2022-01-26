d3c3b7de197ece46b78f6ef32d8253005ee39750
borshasafia <97159895+borshasafia@users.noreply.github.com>
2022-01-26 12:15:02 +0600

    3

--- app2/models.py
+++ app2/models.py
@@ -5,6 +5,10 @@ from django.db.models import Model
 class Feature(models.Model):
     name: models.CharField(max_length=100)
     details: models.CharField(max_length=500)
+    
+    def __str__(self):
+        return self.name
+        
 
 class Malls(models.Model):
     mallname: models.TextField(max_length=100)
@@ -23,3 +27,6 @@ class Malls(models.Model):
     location: models.URLField(max_length=200)
     images: models.ImageField(upload_to='upload_to/')
     mall_description: models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.name
