179b873abee2b9e4990c4eee0c4f9e35a7f1703a
borshasafia <97159895+borshasafia@users.noreply.github.com>
2022-01-26 18:26:51 +0600

    3

--- app2/views.py
+++ app2/views.py
@@ -125,7 +125,7 @@ def add_malls(request):
         floors_11 = request.POST['floors_11']
         floors_12 = request.POST['floors_12']
 
-        shoppingMalls = ShoppingMalls.objects.create(mallname=mallname, product1=product1,product2=product2,product3=product3,product4=product4,product5=product5,product6=product6,product7=product7,link1=link1,link2=link2,link3=link3,link4=link4,division=division,location=location,phone1=phone1,phone2=phone2,phone3=phone3,phone4=phone4,images=images,mall_description=mall_description)
+        shoppingMalls = ShoppingMalls.objects.create(title_new=title,)
         shoppingMalls.save();
         return redirect('/')
     else:
